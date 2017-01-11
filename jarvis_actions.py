c05b3abb353691529e505b29972ebfde36ce5d71
SwagGothamCoder <skiingrunner5879@gmail.com>
2017-01-10 20:17:30 -0700

    Added find_location action
    Jarvis can now respond to location questions by directing the user to google maps.

--- jarvis_actions.py
+++ jarvis_actions.py
@@ -1,6 +1,13 @@
+import webbrowser
+
 def find_location(input_structure, user_input, phrase_structures, data_points):
-    loc = 'place'
-    for i in data_points:
-        if input_structure[i] == 'NNP':
-            loc = user_input[i]
+    loc = ''
+    if data_points:
+        loc = ''.join(user_input[point]+' ' for point in data_points)
+        loc = loc[:-1]
+    print('loc', loc)
+    if not loc:
+        loc = 'The United States'
+    url = "https://www.google.com/maps/place/"+loc.replace(' ', '+')
+    webbrowser.open(url)
     return 'Locating '+loc+'.'
